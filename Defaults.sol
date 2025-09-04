5221a9d0720ab5e9330993cefde8660095716bb0
defaults <192046681+default-net@users.noreply.github.com>
2025-09-04 23:44:37 +0300

    Update Defaults.sol

--- Defaults.sol
+++ Defaults.sol
@@ -2,16 +2,16 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.22;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {TOKEN} from "@openzeppelin/contracts/token/TOKEN/TOKEN.sol";
+import {TOKENPermit} from "@openzeppelin/contracts/token/TOKEN/extensions/TOKENPermit.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @custom:security-contact strewdavid@proton.me
-contract Defaults is ERC20, Ownable, ERC20Permit {
+contract Defaults is TOKEN, Ownable, TokenPermit {
     constructor(address initialOwner)
-        ERC20("defaults", "Polygon ")
+        TOKEN("defaults", "Polygon ")
         Ownable(initialOwner)
-        ERC20Permit("defaults")
+        TOKENPermit("defaults")
     {
         _mint(msg.sender, 10000000000 * 10 ** decimals());
     }
